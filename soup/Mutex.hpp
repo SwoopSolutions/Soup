72c1108af10ec413fa947a3a8b21e3e04a33ab30
Sainan <sainan@calamity.inc>
2024-11-23 08:05:20 +0100

    Simplify Mutex on Windows

--- soup/Mutex.hpp
+++ soup/Mutex.hpp
@@ -8,30 +8,44 @@ NAMESPACE_SOUP
 	{
 	private:
 		RecursiveMutex mutex;
+#if !SOUP_WINDOWS
 		bool locked = false;
+#endif
 
 	public:
 		void lock()
 		{
 			mutex.lock();
+#if SOUP_WINDOWS
+			SOUP_IF_UNLIKELY (mutex.cs.RecursionCount != 1)
+#else
 			SOUP_IF_UNLIKELY (locked)
+#endif
 			{
 				mutex.unlock();
 				soup::throwAssertionFailed("Attempt to obtain recursive lock on non-recursive mutex");
 			}
+#if !SOUP_WINDOWS
 			locked = true;
+#endif
 		}
 
 		[[nodiscard]] bool tryLock()
 		{
 			if (mutex.tryLock())
 			{
+#if SOUP_WINDOWS
+				SOUP_IF_UNLIKELY (mutex.cs.RecursionCount != 1)
+#else
 				SOUP_IF_UNLIKELY (locked)
+#endif
 				{
 					mutex.unlock();
 					soup::throwAssertionFailed("Attempt to obtain recursive lock on non-recursive mutex");
 				}
+#if !SOUP_WINDOWS
 				locked = true;
+#endif
 				return true;
 			}
 			return false;
@@ -39,7 +53,9 @@ NAMESPACE_SOUP
 
 		void unlock() noexcept
 		{
+#if !SOUP_WINDOWS
 			locked = false;
+#endif
 			mutex.unlock();
 		}
 	};
