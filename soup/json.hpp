a8b02d6c24fd75be0f7e0a43ff4e10d45d706eba
Sainan <sainan@calamity.inc>
2025-01-15 08:58:45 +0100

    Remove deprecated JSON functions

--- soup/json.hpp
+++ soup/json.hpp
@@ -21,13 +21,9 @@ NAMESPACE_SOUP
 		// 2. The returned UniquePtr might be default-constructed in case of a parse error, so you'd be dereferencing a nullptr.
 		[[nodiscard]] static UniquePtr<JsonNode> decode(const std::string& data);
 		[[nodiscard]] static UniquePtr<JsonNode> decode(const char*& c);
-		[[deprecated]] static void decode(UniquePtr<JsonNode>& out, const std::string& data) { out = decode(data); }
-		[[deprecated]] static UniquePtr<JsonNode> decodeForDedicatedVariable(const std::string& data) { return decode(data); }
 
 		// specific to soup
 		[[nodiscard]] static UniquePtr<JsonNode> binaryDecode(Reader& r);
-		[[deprecated]] static void binaryDecode(UniquePtr<JsonNode>& out, Reader& r) { out = binaryDecode(r); }
-		[[deprecated]] static UniquePtr<JsonNode> binaryDecodeForDedicatedVariable(Reader& r) { return binaryDecode(r); }
 
 		// internal
 		static void handleLeadingSpace(const char*& c);
