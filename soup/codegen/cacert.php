a97cfcf324da831d9c7e7582e6009bffe5a08b63
Sainan <sainan@calamity.inc>
2025-01-22 12:48:37 +0100

    Compress cacerts

--- soup/codegen/cacert.php
+++ soup/codegen/cacert.php
@@ -1,24 +1,32 @@
 <?php
 // https://curl.se/docs/caextract.html
 $file = file_get_contents("cacert.pem"); // https://curl.se/ca/cacert.pem
-$cert = false;
+$all_certs = "";
+$num_certs = 0;
+$cert = null;
 foreach(explode("\n", $file) as $line)
 {
 	$line = trim($line);
 	if ($line == "-----BEGIN CERTIFICATE-----")
 	{
-		echo "\"";
-		$cert = true;
+		$cert = "";
 		continue;
 	}
 	if ($line == "-----END CERTIFICATE-----")
 	{
-		echo "\",\n";
-		$cert = false;
+		$decoded = base64_decode($cert);
+		(strlen($decoded) <= 0xFFFF) or die("Cert to big");
+		$all_certs .= pack("v", strlen($decoded)).$decoded;
+		++$num_certs;
+		$cert = null;
 		continue;
 	}
-	if ($cert)
+	if ($cert !== null)
 	{
-		echo $line;
+		$cert .= $line;
 	}
 }
+($num_certs <= 0xFFFF) or die("Too many certs");
+$all_certs = pack("v", $num_certs).$all_certs;
+$bin_str = gzdeflate($all_certs);
+file_put_contents("../cacerts.hpp", "static const char cacerts[] = { '\\x".join("', '\\x", array_map("dechex", array_map("ord", str_split($bin_str))))."' };");
